f73188fc4bad57db5d9a4fe57d280702fb2322ac
Dave Gööck <dave@goeoeck.net>
2022-10-08 17:53:55 +0000

    play around with hmr config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,12 @@ export default defineNuxtConfig({
         },
       },
     },
+    server: {
+      hmr: {
+        // protocol: 'wss',
+        // clientPort: 443,
+      },
+    },
   },
   build: {
     postcss: {
